f149de7654cd6de38870e7920a35cdd612953f35
Burbonio <burbon.93@mail.ru>
2016-01-24 16:24:07 +0100

    First commit

---
+++ src/lesson1/LessonOne.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+public class LessonOne {
+	private static String a;
+
+	public static void main(String[] args) {
+		
+		a = "Hello";
+		System.out.println(a);
+		
+		
+
+	}
+
+}
