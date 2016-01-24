a190debda7e5fa8078a2deb82ff405bc52c1c4dd
Burbonio <burbon.93@mail.ru>
2016-01-24 16:50:32 +0100

    Second commit

--- src/lesson1/LessonOne.java
+++ src/lesson1/LessonOne.java
@@ -1,9 +1,31 @@
 package lesson1;
 
+/**
+ * Lesson
+ * @author avgdi
+ *
+ */
 public class LessonOne {
 	private static String a;
 
+	/**
+	 * Main method for 1st lesson
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		//celocislennye
+		byte bigByte=-128;
+		short bigShort=32767;
+		int bigInt=2100000000;
+		long bigLong=12L; // letter help to describe the type of variable 
+		//drobi
+		float bigFloat=3.14F;// 6 decimal places 
+		double bigDouble=3.14D; // 15 decimal places 
+		//boolean 
+		boolean trueOrFalse=true;
+		char randomCharacter='A';
+		String slovo="Word";
+		System.out.println(slovo.length());
 		
 		a = "Hello";
 		System.out.println(a);
